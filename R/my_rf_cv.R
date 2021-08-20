43c82691ad219bb3754b9e0efd23520949b2d077
Kelsi Osorio <kelsio3101@gmail.com>
2021-08-20 02:46:25 -0700

    Adding my_rf_cv documentation

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -1,20 +1,39 @@
+#' Random Forest Cross-Validation
+#'
+#' This function predicts output body_mass_g using covariates bill_length_mm, bill_depth_mm, and flipper_length_mm.
+#'
+#' @param k Number of folds
+#' @keywords prediction
+#'
+#' @return A numeric with the cross validation error
+#'
+#' @examples
+#' my_rf_cv(5)
+#' my_rf_cv(7)
+#'
+#' @export
 my_rf_cv <- function(k) {
+  # Clean data
+  clean_penguins <- stats::na.omit(STAT302package::my_penguins %>% dplyr::select(bill_length_mm,
+                                                                              bill_depth_mm,
+                                                                              flipper_length_mm,
+                                                                              body_mass_g))
   # split data into k parts, randomly
-  fold <- sample(rep(1:k, length = nrow(data_clean2)))
+  fold <- sample(rep(1:k, length = nrow(clean_penguins)))
   # binding data together
-  data <- cbind(data_clean2, fold)
+  data <- cbind(clean_penguins, fold)
   # initializing data frame with errors
   mse_df <- data.frame()
   for (i in 1:k) {
     # splitting into training and testing data
-    data_train <- data %>% filter(fold != i)
-    data_test <- data %>% filter(fold == i)
+    data_train <- data %>% dplyr::filter(fold != i)
+    data_test <- data %>% dplyr::filter(fold == i)
     # running a random forest model
-    model <- randomForest(body_mass_g ~ bill_length_mm + bill_depth_mm + flipper_length_mm,
-                          data = data_train,
-                          ntree = 100)
+    model <- randomForest::randomForest(body_mass_g ~ bill_length_mm + bill_depth_mm + flipper_length_mm,
+                                        data = data_train,
+                                        ntree = 100)
     # finding predictions
-    predictions <- predict(model, data_test[, -4])
+    predictions <- stats::predict(model, data_test[, -4])
     # calculating MSE
     mse <- sum((predictions - data_test$body_mass_g)^2) / nrow(data_test)
     # adding that number to table
@@ -26,3 +45,5 @@ my_rf_cv <- function(k) {
   CV_MSE <- colMeans(mse_df)
   return(CV_MSE)
 }
+
+utils::globalVariables(c("bill_length_mm", "bill_depth_mm", "body_mass_g", "flipper_length_mm"))
