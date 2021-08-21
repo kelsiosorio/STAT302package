49e5e23acb6ed7cac5d11c4f0b93b2cd26aacf97
Kelsi Osorio <kelsio3101@gmail.com>
2021-08-20 23:47:25 -0700

    Adding test unit for my_lm

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# within test-my_lm.R
+library(STAT302package)
+
+
+my_data <- data.frame("x" = c(4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14),
+                      "y" = c(5.34, 6.78, 4.78, 8.45, 5.81, 6.24, 6.93, 5.88, 6.46, 7.12))
+#Fitting model
+my_lm(x ~ y, data = my_data)
+
+test_that("Expecting data frame", {
+  expect_type(my_lm(x ~ y, data = my_data), "list")
 })
+
