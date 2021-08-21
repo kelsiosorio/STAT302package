7b47d15478a8f9e892f325bd5cab11f279bd34e6
Kelsi Osorio <kelsio3101@gmail.com>
2021-08-21 01:54:20 -0700

    Adding unit test for my_knn_cv

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# within test-my_knn_cv.R
+library(STAT302package)
+# Getting data ready
+data("my_penguins")
+data_clean <- stats::na.omit(my_penguins %>% dplyr::select(species, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g))
+
+# filtering for training data
+p_train <- data_clean %>% dplyr::select(bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g)
+
+# Using function
+knn_1 <- my_knn_cv(p_train, data_clean$species, 1, 5)
+
+test_that("Expecting List", {
+  expect_type(knn_1, "list")
 })
